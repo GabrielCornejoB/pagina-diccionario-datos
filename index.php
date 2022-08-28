13a52328998604dfc442659fdd13e7ff6acf642c
Gabriel <gabrielcorbot@gmail.com>
2022-08-28 12:42:01 -0500

    Navs se generan por php

--- index.php
+++ index.php
@@ -14,18 +14,13 @@
 <body>
     <header class="navbar">
         <h1 class="header-title">DICCIONARIO DE DATOS</h2>
-        <button class="hamburger-menu">
-            <div class="bar"></div>
-        </button>
+        <?php
+            generate_nav();
+        ?>
     </header>
-    <nav class="navbar-2">
-        <a href='#'>Términos de negocio</a>
-        <a href='#'>Atríbutos / Datos</a>
-        <a href='#'>Fuentes de Datos</a>
-        <a href='#'>Objetivos</a>
-        <a href='#'>Procesos</a>
-        <a href='#'>Riesgos</a>
-    </nav>
+    <?php
+        generate_hamburger_menu();
+    ?>
     <script src="./js/main.js"></script>
 </body>
 </html>
--- php/functions.php
+++ php/functions.php
@@ -1,4 +1,16 @@
 <?php
-function generateNav() {
-    
+function generate_nav() {
+    echo "<button class='hamburger-menu'>" .
+            "<div class='bar'></div>" .
+        "</button>";
+}
+function generate_hamburger_menu() {
+    echo "<nav class='navbar-2'>" .
+            "<a href='#'>Términos de negocio</a>" .
+            "<a href='#'>Atríbutos / Datos</a>" . 
+            "<a href='#'>Fuentes de Datos</a>" .
+            "<a href='#'>Objetivos</a>" .
+            "<a href='#'>Procesos</a>" .
+            "<a href='#'>Riesgos</a>" .
+        "</nav>";
 }
